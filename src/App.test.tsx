03f288e793243219c91a54f0c8ada56f91ef7920
Mohammad-Haseeb <mohammad.haseeb310@gmail.com>
2021-01-18 20:50:33 +0500

    Basic Structure

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,6 @@ import App from "./App";
 describe("App", () => {
 	it("should render a <div />", () => {
 		const container = shallow(<App />);
-		expect(container.find("div").length).toEqual(1);
+		expect(container.find("div").length).toBeGreaterThanOrEqual(1);
 });
 });
---
+++ src/Component/TimeDescription.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function TimeDescription(){
+    return(
+        <> 
+             <h3 className="timeShow">25 : 46</h3>
+        </>
+    );
+}
