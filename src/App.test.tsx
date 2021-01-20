f041a5a23093e0059c0d87faeb7ded66a2a2fa77
Mohammad-Haseeb <mohammad.haseeb310@gmail.com>
2021-01-21 01:29:42 +0500

    test pass

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import App from "./App";
+import App from "./Component/App/App";
 
 describe("App", () => {
 	it("should render a <div />", () => {
